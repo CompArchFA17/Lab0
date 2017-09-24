61511c75c24790332e87f0c5343b31a710f5f4ae
dpapp <david.papp@students.olin.edu>
2017-09-24 17:02:49 -0400

    starting writeup

--- adder.t.v
+++ adder.t.v
@@ -16,20 +16,39 @@ module testFullAdder4bit();
       $display("A    B     | Sum  Cout Overflow ");
       a=4'b0000;b=4'b0000; #1000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
-      a=4'b0001;b=4'b0001; #1000;
+      a=4'b011;b=4'b0011; #1000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
-      a=4'b0011;b=4'b0000; #1000;
+      /*a=4'b0011;b=4'b0000; #1000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
       a=4'b0000;b=4'b0011; #10000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
       a=4'b0101;b=4'b0010; #10000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
-      a=4'b0101;b=4'b0011; #10000;
+      a=4'b0001;b=4'b0011; #10000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
-      a=4'b1001;b=4'b1110; #10000;
+      a=4'b0010;b=4'b1010; #10000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
       a=4'b1101;b=4'b1011; #10000;
       $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
+
+      a=4'b1101;b=4'b0110; #10000; 
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow); // 0011 0 0
+      a=4'b1110;b=4'b0010; #10000;
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow); // 0000 0 0
+      a=4'b0001;b=4'b0110; #10000; 
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow); // 0111 0 0
+      a=4'b1000;b=4'b0010; #10000;
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow); // 0110 0 0
+
+      // Overflow tests:
+      a=4'b1000;b=4'b1001; #10000;
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
+      a=4'b1011;b=4'b1100; #10000;
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
+      a=4'b0101;b=4'b0100; #10000;
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);
+      a=4'b0010;b=4'b0111; #10000;
+      $display("%b %b  | %b %b    %b", a, b, sum, carryout, overflow);*/
       $finish;
     end
 endmodule
