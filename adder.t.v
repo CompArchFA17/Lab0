ec9b8aaff12d652dc6e29117f40a071e2fcc35c1
Sung Park <sungwoo.park@students.olin.edu>
2017-09-25 21:58:47 -0400

    Add comments to the files

--- adder.t.v
+++ adder.t.v
@@ -15,15 +15,23 @@ module testFullAdder();
         $dumpfile("adder.vcd");
         $dumpvars(0,testFullAdder);
         // Your test code here
+        
+        $display("general case for no carryout and no overflow");
         $display("a0 a1 a2 a3 b0 b1 b2 b3 | s0 s1 s2 s3 | carryout overflow");
         a=4'b0000;b=4'b0101; #1000
         $display("%b  %b  %b  %b  %b  %b  %b  %b  | %b  %b  %b  %b  |        %b        %b", a[0], a[1], a[2], a[3], b[0], b[1], b[2], b[3], s[0], s[1], s[2], s[3], carryout, overflow);
         a=4'b0011;b=4'b0001; #1000
         $display("%b  %b  %b  %b  %b  %b  %b  %b  | %b  %b  %b  %b  |        %b        %b", a[0], a[1], a[2], a[3], b[0], b[1], b[2], b[3], s[0], s[1], s[2], s[3], carryout, overflow);
+        
+        $display("case with overflow and no carryout");
         a=4'b0101;b=4'b0011; #1000
         $display("%b  %b  %b  %b  %b  %b  %b  %b  | %b  %b  %b  %b  |        %b        %b", a[0], a[1], a[2], a[3], b[0], b[1], b[2], b[3], s[0], s[1], s[2], s[3], carryout, overflow);
-        a=4'b1001;b=4'b1110; #1000
+
+        $display("case with overflow and carrayout");
+        a=4'b1001;b=4'b1110; #1000 
         $display("%b  %b  %b  %b  %b  %b  %b  %b  | %b  %b  %b  %b  |        %b        %b", a[0], a[1], a[2], a[3], b[0], b[1], b[2], b[3], s[0], s[1], s[2], s[3], carryout, overflow);
+
+        $display("case with no overblow but carryout");
         a=4'b1101;b=4'b1011; #1000
         $display("%b  %b  %b  %b  %b  %b  %b  %b  | %b  %b  %b  %b  |        %b        %b", a[0], a[1], a[2], a[3], b[0], b[1], b[2], b[3], s[0], s[1], s[2], s[3], carryout, overflow);
     end
--- adder.v
+++ adder.v
@@ -57,14 +57,17 @@ module FullAdder4bit
   input[3:0] b      // Second operand in 2's complement format
 );
     // Your Code Here
+    // carryin wires
     wire carryin1;
     wire carryin2;
     wire carryin3;
     
+    // 1-bit adder
     structuralFullAdder adder0(sum[0],carryin1,a[0],b[0],0);
     structuralFullAdder adder1(sum[1],carryin2,a[1],b[1],carryin1);
     structuralFullAdder adder2(sum[2],carryin3,a[2],b[2],carryin2);
     structuralFullAdder adder3(sum[3],carryout,a[3],b[3],carryin3);
 
+    // xor gate for determining overflow
     `XOR overflowxorgate(overflow, carryin3, carryout);
 endmodule
